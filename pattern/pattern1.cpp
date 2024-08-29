3a8f35ffbe7f80b2a574742548063f7d0bc08ebd
aryan <aryankumarchallenger@gmail.com>
2024-08-29 11:41:28 +0530

    pattern10and12

--- pattern/pattern1.cpp
+++ pattern/pattern1.cpp
@@ -102,13 +102,90 @@ void pattern9(int a){
     }
 }
 
+void pattern10(int a){
+    for(int i = 0;i<a;i++){
+        for(int j=0;j<a-i-1;j++){
+           cout<<" ";
+        }
+        for(int j=0;j<(2*i)+1;j++){
+           cout<<"*";
+        }
+         for(int j=0;j<a-i-1;j++){
+           cout<<" ";
+        }
 
+        cout<<endl;
+    }
+
+    for(int i = 0;i<a-1;i++){
+        for(int j = 0;j<=i;j++){
+           cout<<" ";
+        }
+        for(int j = 0;j<(2*(a-i))-3;j++){
+            cout<<"*";
+        }
+         for(int j = 0;j<=i;j++){
+           cout<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+
+void pattern11(int a){
+    for(int i = 0;i<a;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=0;i<a-1;i++){
+        for(int j=0;j<(a-i)-1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+// Now using if case 
+
+void P11remake(int a){
+    for(int i = 1;i<=(2*a)-1;i++){
+        if(i>a){
+            for(int j=0;j<(2*a)-i;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+        else{
+            for(int j=0;j<i;j++){
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+    }
+}
+
+
+void pattern12(int a){
+    for(int i = 1;i<=a;i++){
+        for(int j = 1;j<=i;j++){
+            if((i+j)%2 == 0){
+                cout<<"1";
+            }
+            else{
+                cout<<"0";
+            }
+        }
+        cout<<endl;
+    }
+}
 
 int main(){
  int a;
  cout<<"Enter the no. ";
  cin>>a;
- pattern9(a);
+ pattern12(a);
  return 0;
 }
 
