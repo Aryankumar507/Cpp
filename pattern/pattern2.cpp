8824aab0ec8c33cf140a5e0cde86a979419e3cae
Aryan Kumar <aryankumarchallenger4@gmail.com>
2024-09-03 01:31:02 +0530

    all patterns are completed

--- pattern/pattern2.cpp
+++ pattern/pattern2.cpp
@@ -53,10 +53,121 @@ void pattern18(int a){
     }
 }
 
+void pattern19(int a){
+    for(int i=0;i<a;i++){
+        for(int j=0;j<=i;j++){
+            char ch = 'A';
+            ch = ch + (a-1);
+            ch = ch-(i-j);
+            cout<<ch<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+
+void pattern20(int a){
+   for(int i = 0;i<a;i++){
+    for(int j = 0;j<a-i;j++){
+        cout<<"*";
+    }
+    for(int j = 0;j<2*i;j++){
+        cout<<" ";
+    }
+     for(int j = 0;j<a-i;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+   }
+   for(int i = 0;i<a;i++){
+    for(int j = 0;j<=i;j++){
+        cout<<"*";
+    }
+
+    for(int j = 0;j<(a-i-1)*2;j++){
+        cout<<" ";
+    }
+
+    for(int j = 0;j<=i;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+   }
+}
+
+
+void pattern21(int a){
+    for(int i = 0;i<a;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        for(int j = 0;j<(a-i-1)*2;j++){
+            cout<<" ";
+        }
+          for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=0;i<a-1;i++){
+        for(int j = 0;j<(a-i)-1;j++){
+            cout<<"*";
+        }
+        for(int j = 0;j<(i+1)*2;j++){
+        cout<<" ";
+        }
+         for(int j = 0;j<(a-i)-1;j++){
+            cout<<"*";
+        }
+          cout<<endl;
+    }
+}
+
+void pattern22(int a){
+    for(int i = 0;i<a;i++){
+        for(int j = 0;j<a;j++){
+            if(i==0||i==a-1||j==0||j==a-1){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
+void pattern23(int a){
+    if(a<3||(a%2)==0){
+        cout<<"Enter any odd no greater than 2";
+    }
+    else{
+        for(int i = 0;i<a;i++){
+            for(int j = 0;j<a;j++){
+                if(i==0||i==a-1||j==0||j==a-1){
+                    cout<<((a+1)/2);
+                }
+                else if(i==1||i==a-2||j==1||j==a-2){
+                   int m = (a-1)/2;
+                    cout<<m;
+                }
+                else if(i==2||i==a-3||j==2||j==a-3){
+                    int m1=(a-3)/2;
+                    cout<<m1;
+                }
+                else{
+                    cout<<"1";
+                }
+            }
+            cout<<endl;
+        }
+    }
+}
+
 int main(){
     int a;
     cout<<"Enter the no. ";
     cin>>a;
-    pattern18(a);
+    pattern23(a);
     return 0;
 }
