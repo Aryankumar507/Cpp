27a6730fc37acecbf54be84ebf0077b032c59099
Aryan Kumar <aryankumarchallenger4@gmail.com>
2024-09-10 03:37:45 +0530

    some basic concept of recursion

---
+++ .vscode/recursion/recur1.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+What is recursion ?
+-> when a function calls itself until a specified condition is met.
+
+// Stack overflow: A stack overflow in recursion occurs when a
+ recursive function makes too many nested calls, exceeding the 
+ call stack's capacity. Each recursive call uses some space on
+ the stack to store information like function parameters,
+ return addresses, and local variables. If the recursion is too deep,
+ the stack can run out of space, leading to a stack overflow error.//
+
+ // Base condition : condition which is used for stoping the stack over flow.
+
+ // Recursion Tree :  recursion tree is a useful visual tool to understand
+  and analyze the behavior of a recursive function, particularly its time 
+  and space complexity. It helps in visualizing how a recursive function
+  breaks down a problem into smaller subproblems and how those subproblems
+  relate to each other.
+  // its a method for visualising the recursion.
+*/
+int main(){
+    
+}
